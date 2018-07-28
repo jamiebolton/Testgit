33b9842ad04ccb47d2ccd123f007e34fe508032d
crazypenguin <crazypenguin@aliyun.com>
2018-07-28 22:42:25 +0800

    commit happy

---
+++ src/club/crazypenguin/demo/Happy.java
@@ -0,0 +1,24 @@
+/*
+ * File name:          Happy.java
+ * Copyright@Wuxi Information Technology Inc (China)
+ * Editor:           JDK1.7.0_79
+ */
+package club.crazypenguin.demo;
+
+/**
+ * TODO: File comments
+ * <p>
+ * <p>
+ * Author:           crazypenguin
+ * <p>
+ * Date:           2018年7月28日
+ * <p>
+ * Time:           下午10:42:11
+ * <p>
+ * Director:         crazypenguin
+ * <p>
+ * <p>
+ */
+public class Happy {
+
+}
