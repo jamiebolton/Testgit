7ebe641f5c8a143d0cf387461614f312a5ec8b26
crazypenguin <crazypenguin@aliyun.com>
2018-07-28 22:41:39 +0800

    123

---
+++ src/club/crazypenguin/demo/User.java
@@ -0,0 +1,42 @@
+/*
+ * File name:          User.java
+ * Copyright@Wuxi Information Technology Inc (China)
+ * Editor:           JDK1.7.0_79
+ */
+package club.crazypenguin.demo;
+
+/**
+ * TODO: File comments
+ * <p>
+ * <p>
+ * Author:           crazypenguin
+ * <p>
+ * Date:           2018年7月28日
+ * <p>
+ * Time:           下午10:07:09
+ * <p>
+ * Director:         crazypenguin
+ * <p>
+ * <p>
+ */
+public class User {
+	private String username;
+	private String pasword;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPasword() {
+		return pasword;
+	}
+
+	public void setPasword(String pasword) {
+		this.pasword = pasword;
+	}
+
+}
